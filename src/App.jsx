7c1b2fc282ada638b45e5715e1c325d0a1c4c6ba
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2025-11-27 10:38:12 +0200

    Add react-router and set up basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router";
 
 function App() {
 
 	return (
 		<>
 			<h1>test</h1>
+			<Routes>
+				<Route path="/" element={<h2>Home</h2>} />
+			</Routes>
 		</>
 	)
 }
